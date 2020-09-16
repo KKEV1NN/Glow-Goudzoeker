3d51c864992b58c71f913b5e79ea5fe667d552e1
JeroenKoolen <48135261+JeroenKoolen@users.noreply.github.com>
2020-09-16 10:33:25 +0200

    removed timer added timer

--- Glow/Assets/vote.cs
+++ Glow/Assets/vote.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class vote : MonoBehaviour
 {
+    public bool isCounting;
+    public float timer = 100;
     List<string> votes = new List<string>();
     void Start()
     {
@@ -13,6 +15,13 @@ public class vote : MonoBehaviour
 
     private void Update()
     {
+        Debug.Log(timer);
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            Sort()
+            Debug.Log("cast votes");
+        }
     }
 
     public void Voted(string s)
@@ -20,65 +29,8 @@ public class vote : MonoBehaviour
         votes.Add(s);
     }
 
-    //private void 
-
-     public IEnumerator Count( // temp 
-     float targetTime,
-     System.Action<float> receiveTimeElapsed = null,
-     System.Func<bool> actionCondition = null,
-     System.Action conditionalAction = null,
-     System.Action onCountStart = null,
-     System.Action onCountEnd = null)
+    private void Sort()
     {
-        onCountStart?.Invoke();
-
-        float timeElapsed = 0f;
-        receiveTimeElapsed?.Invoke(timeElapsed);
-
-        while (timeElapsed < targetTime)
-        {
-            if (actionCondition != null && actionCondition.Invoke())
-                conditionalAction?.Invoke();
-
-            yield return null;
-
-            timeElapsed += Time.deltaTime;
-
-            receiveTimeElapsed?.Invoke(timeElapsed);
-        }
-
-        if (actionCondition != null && actionCondition.Invoke())
-            conditionalAction?.Invoke();
-
-        onCountEnd?.Invoke();
     }
 
-    private float _timeElapsed;
-    private int _count;
-
-    private void Awake()
-    {
-        this.StartCoroutine(
-            this.Count(
-                5f,
-                (timeElapsed) =>
-                {
-                    this._timeElapsed = timeElapsed;
-                    Debug.Log("Time Elapsed: " + this._timeElapsed);
-                },
-                () => { return this._timeElapsed >= (this._count + 1); },
-                () =>
-                {
-                    this._count++;
-
-                    Debug.Log("Countdown: " + this._count);
-                },
-                () => { Debug.Log("Countdown: " + this._count); },
-                () => {
-                    Debug.Log("On countdown end");
-                    // 
-                }
-            )
-        );
-    }
 }
